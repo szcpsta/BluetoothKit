4d82bfe48d593031ec46332ef7928ba681076527
szcpsta <khahn0721@gmail.com>
2026-01-15 01:41:32 +0900

    Refactoring (#3)
    Co-authored-by: kihun.ahn <kihun.ahn@samsung.com>

--- BtSnoop/Hci/HciPackets.cs
+++ BtSnoop/Hci/HciPackets.cs
@@ -1,7 +1,7 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-namespace BtSnoop.Parser;
+namespace BtSnoop.Hci;
 
 public abstract class HciPacket
 {
